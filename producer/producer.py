ea4cf2d2d32b68153c88f3a541ec19722f42257a
surya-raj-02 <suryasandeepraj2002@gmail.com>
2022-03-18 09:30:25 +0530

    Did basic producer

---
+++ producer/producer.py
@@ -0,0 +1,40 @@
+# Code for Producer
+from flask import Flask
+from flask import request
+import json
+
+global consumer_data
+app = Flask(__name__)
+cunsumer_data = []
+
+
+@app.route('/new_ride',methods=["POST"])
+def new_ride():
+    my_new_string_value = request.data.decode("utf-8")
+    data = json.loads(my_new_string_value)
+    pickup = data['pickup']
+    destination = data['destination']
+    time = data['time']
+    cost = data['cost']
+    seats = data['seats']
+    return "Got data"
+
+@app.route("/new_ride_matching_consumer",methods=["POST"])
+def new_ride_matching_consumer():
+    my_new_string_value = request.data.decode("utf-8")
+    data = json.loads(my_new_string_value)
+    consumer_id = data['consumer_id']
+    ip = data['ip']
+    req_ip = data['req_ip']
+    name = data['name']
+    mapp = dict()
+    mapp[name,ip] = [consumer_id,req_ip]
+    consumer_data.append(mapp)
+
+@app.route("/home")
+def home():
+    global cunsumer_data
+    print(consumer_data)
+    return str(consumer_data)
+if __name__ == '__main__':
+   app.run(debug = True)
